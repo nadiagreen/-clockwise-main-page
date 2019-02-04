fcd2c90955d39e788be095e3780f13272af2fddb
nadiagreen <nadiagreen1960@gmail.com>
2019-02-04 18:49:45 +0200

    Initial commit.

--- ex3.js
+++ ex3.js
@@ -1,4 +1,4 @@
-var open = document.getElementById("open");
+ var open = document.getElementById("open");
 var div1 = document.getElementById("div1");
 var div2 = document.getElementById("div2");
 var div3 = document.getElementById("div3");
@@ -102,7 +102,7 @@ function myFunction (){
         }
     }
 }
-}
+} 
  
                                  
   
