297a8df9693b1e3e3264d080437c6cba273fef87
nadiagreen <nadiagreen1960@gmail.com>
2019-02-13 20:50:18 +0200

    Eighth commit.

--- js.js
+++ js.js
@@ -5,6 +5,12 @@ var div3 = document.getElementById("div3");
 var drop = document.getElementById("drop");
 function myFunction (){
     if( drop.style.height == 0 || div2.style.width == "25px"){
+        open1();
+    }else if(drop.style.height == "350px" || div2.style.width == 0){
+        close();
+    }
+}
+    function open1(){
     var pos1 = 5;
     var angle1 = 0;
     var id1 = setInterval(frame1, 100);
@@ -52,8 +58,9 @@ function myFunction (){
             drop.style.height = height4 + "px";
         }
     }
-}else { 
-    if(drop.style.height == "350px" || div2.style.width == 0) {
+    }
+ 
+    function close(){     
     var pos1 = 10;
     var angle1 = 45;
     var id1 = setInterval(frame1, 100);
@@ -102,9 +109,65 @@ function myFunction (){
         }
     }
 }
-}    
+ function myFu(x) {
+  if (x.matches && drop.style.height == "350px") { // If media query matches
+      close1();
+  } 
 }
-  
+var x = window.matchMedia("(min-width: 900px)");
+myFu(x); // Call listener function at run time
+x.addListener(myFu); // Attach listener function on state changes 
+
+   function close1(){     
+    var pos1 = 10;
+    var angle1 = 45;
+    var id1 = setInterval(frame1, 10);
+    function frame1(){
+    if(pos1 == 5 || angle1 == 0) {
+        clearInterval(id1);
+    }else {
+        pos1--;
+        angle1 -= 9;
+        div1.style.top = pos1 + "px";
+        div1.style.transform = 'rotate( '+angle1+'deg)';
+         }
+    }
+    var pos3 = 10;
+    var angle3 = -45;
+    var id3 = setInterval(frame3, 10);
+    function frame3(){
+    if(pos3 == 15 || angle3 == 0) {
+        clearInterval(id3);
+    }else {
+        pos3++;
+        angle3 +=9;
+        div3.style.top = pos3 + "px";
+        div3.style.transform = 'rotate( '+angle3+'deg)';
+         }
+    }
+    var width2 = 0;
+    var id2 = setInterval(frame2, 10);
+    function frame2 (){
+        if(width2 == 25){
+            clearInterval(id2);
+        } else{
+            width2 += 5;
+            div2.style.width = width2 + "px";
+        }
+    }
+   
+ var height4 = 350;
+    var id4 = setInterval(frame4, 1);
+    function frame4 (){
+        if(height4 == 0){
+            clearInterval(id4);
+        } else{
+            height4 -= 7;
+            drop.style.height = height4 + "px";
+        }
+    }
+}
+ 
                            
   
 
