6a807c785ad07ee093821407f583956451aee73f
nadiagreen <nadiagreen>
2019-03-06 18:58:37 +0200

    Envelope and validation.

--- js/form.js
+++ js/form.js
@@ -2,6 +2,8 @@
 var dwsContainer = document.querySelector('.dws-container');
 var inputs = document.getElementsByClassName("userInput");
 var errors = document.getElementsByClassName("error");
+var email = document.getElementsByClassName("email");
+
 inputs[0].addEventListener("blur",function(){
     if(inputs[0].value == "") {
         errors[0].style.display = "block";
@@ -15,7 +17,7 @@ inputs[1].addEventListener("blur", function(){
         errors[1].style.zIndex = "1";
     }else{errors[1].style.display = "none";}
  });
-inputs[2].addEventListener("click", function(){
+inputs[2].addEventListener("blur", function(){
     if(inputs[2].value == "") {
         errors[2].style.display = "block";
         errors[2].style.zIndex = "1";
@@ -31,4 +33,22 @@ inputs[2].addEventListener("click", function(){
            errors[i].style.display = "none";
        }
   };  
+ 
+function validateEmail() {
+  var str = inputs[1].value;
+  var patt = /\S+@\S+\.\S+/;
+  var result = str.match(patt);
+    if (result != str && inputs[1].value != ""){
+        errors[1].innerHTML = "Wrong E-mail";
+        errors[1].style.zIndex = "1";
+        errors[1].style.display = "block";
+    }
+     if(str == "") {
+         errors[1].innerHTML = "Please provide your e-mail";
+        errors[1].style.zIndex = "1";
+        errors[1].style.display = "block";
+    }
+    if(inputs[0].value != ""){errors[0].style.display = "none";}
+    if(inputs[2].value != ""){errors[2].style.display = "none";}
+} 
  
