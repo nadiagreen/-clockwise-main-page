0db10aebef43900625a713702330d010ec5f01c9
nadiagreen <nadiagreen>
2019-03-08 22:01:25 +0200

    Input keyup.

--- js/form.js
+++ js/form.js
@@ -2,57 +2,52 @@
 var dwsContainer = document.querySelector('.dws-container');
 var inputs = document.getElementsByClassName("userInput");
 var errors = document.getElementsByClassName("error");
-var email = document.getElementsByClassName("email");
 
-inputs[0].addEventListener("blur",function(){
-    if(inputs[0].value == "") {
-        errors[0].style.display = "block";
-        errors[0].style.zIndex = "1";
-    }else{errors[0].style.display = "none";}
- });
- 
-inputs[1].addEventListener("blur", function(){
-    if(inputs[1].value == "") {
-        errors[1].style.display = "block";
-        errors[1].style.zIndex = "1";
-    }else{errors[1].style.display = "none";}
- });
-inputs[2].addEventListener("blur", function(){
-    if(inputs[2].value == "") {
-        errors[2].style.display = "block";
-        errors[2].style.zIndex = "1";
-    }else {errors[2].style.display = "none";}
- });
- function visible(){
+function visible(){
      dwsContainer.style.display = "block"; 
  }
-   function invisible(){
+
+ function invisible(){
       dwsContainer.style.display = "none";
        for (var i = 0; i < inputs.length; i++){
            inputs[i].value = "";
            errors[i].style.display = "none";
        }
-  };  
- 
- 
-function validateEmail() {
+  }
+
+inputs[0].addEventListener("keyup", function(){
+         if(inputs[0].value == ""){
+             errors[0].style.display = "block";
+         }else errors[0].style.display = "none";
+     });
+
+ inputs[1].addEventListener("keyup", function(){
+         if(inputs[1].value == ""){
+             errors[1].style.display = "block";
+             errors[1].innerHTML = "Provide Your E-mail";
+         }else {
+             val();
+         }
+     });     
+  function val() {
   var str = inputs[1].value;
-  var patt = /\S+@\S+\.\S+/;
+  var  patt = "[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$" ;
   var result = str.match(patt);
-    if (result != str && inputs[1].value != ""){
-        errors[1].innerHTML = "Wrong E-mail";
-        errors[1].style.zIndex = "1";
-        errors[1].style.display = "block";
+  if(result != str && str != "") {
+       errors[1].style.display = "block";
+      errors[1].innerHTML = "Wrong e-mail";   
+  } else if (str == ""){
+      errors[i].innerHTML = "please provide your e-mail";
+  }
+    else if (result == str){
+        errors[1].style.display = "none";
     }
-     if(str == "") {
-         errors[1].innerHTML = "Please provide your e-mail";
-        errors[1].style.zIndex = "1";
-        errors[1].style.display = "block";
-    }
-    if(inputs[0].value != ""){errors[0].style.display = "none";}
-    if(inputs[2].value != ""){errors[2].style.display = "none";}
-} 
-function fValidate(){
-    if(inputs[0].value != ""){errors[0].style.display = "none";}
-    if(inputs[2].value != ""){errors[2].style.display = "none";}
-} 
+}
+
+inputs[2].addEventListener("keyup", function(){
+    if(inputs[2].value == ""){
+        errors[2].style.display = "block";
+    }else errors[2].style.display = "none";
+});
+ 
+
